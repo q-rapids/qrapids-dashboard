933df98929fe0d26b23187070710f2bbadae4bc2
Aleix Balletbó <aleixballetbo@gmail.com>
2019-05-21 10:32:27 +0200

    Load projects from SI indexes

--- src/main/resources/static/js/projectsSelector.js
+++ src/main/resources/static/js/projectsSelector.js
@@ -20,8 +20,8 @@ XMLHttpRequest.prototype.open = (function(open) {
     return function(method,url,async) {
         if (url.search("/api") !== -1 && url.search("/api/assessedProjects") === -1 && url.search("/serverUrl") === -1 && url.search("/me") === -1) {
             var prj = sessionStorage.getItem("prj");
-            console.log(url+"Project: "+prj);
-            if (!prj)
+            console.log(url+" Project: "+prj);
+            if (!prj || prj === " ")
                 getProjects();
             else {
                 url = setQueryStringParameter(url, "prj", prj);
@@ -35,14 +35,16 @@ XMLHttpRequest.prototype.open = (function(open) {
 })(XMLHttpRequest.prototype.open);
 
 function getProjects() {
-    jQuery.ajax({
-        dataType: "json",
-        url: "../api/assessedProjects",
-        cache: false,
-        type: "GET",
-        async: true,
-        success: function (data) {
-            if (!sessionStorage.getItem("projects")) {
+    if (sessionStorage.getItem("projects") && sessionStorage.getItem("projects").length > 0) {
+        showProjectSelector(projects);
+    } else {
+        jQuery.ajax({
+            dataType: "json",
+            url: "../api/assessedProjects",
+            cache: false,
+            type: "GET",
+            async: false,
+            success: function (data) {
                 sessionStorage.setItem("projects", JSON.stringify(data));
                 if (data.length === 0) { //For testing purposes
                     setProject(" ");
@@ -51,27 +53,30 @@ function getProjects() {
                     setProject(data[0]);
                 }
                 else {
-
-                    for (var i = 0; i < data.length; i++) {
-                        $("#projectsModalItems").append('<button class="list-group-item">' + data[i] + '</button>');
-                    }
-
-                    $('.list-group-item').on('click', function () {
-                        var $this = $(this);
-
-                        $('.active').removeClass('active');
-                        $this.toggleClass('active');
-
-                        $("#projectsModal").modal('hide');
-
-                        setProject($this.text());
-                    });
-
-                    $("#projectsModal").modal();
+                    showProjectSelector(data);
                 }
             }
-        }
+        });
+    }
+}
+
+function showProjectSelector (projects) {
+    for (var i = 0; i < projects.length; i++) {
+        $("#projectsModalItems").append('<button class="list-group-item">' + projects[i] + '</button>');
+    }
+
+    $('.list-group-item').on('click', function () {
+        var $this = $(this);
+
+        $('.active').removeClass('active');
+        $this.toggleClass('active');
+
+        $("#projectsModal").modal('hide');
+
+        setProject($this.text());
     });
+
+    $("#projectsModal").modal();
 }
 
 function setQueryStringParameter(uri, key, value) {
