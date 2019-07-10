189b68ae6a46e0e8fc8e133c4b3203697ab48c25
Aleix Balletbó <aleixballetbo@gmail.com>
2019-07-10 19:52:44 +0200

    Add SonarCloud integration

---
+++ src/test/java/com/upc/gessi/qrapids/app/domain/services/StrategicIndicatorsTest.java
@@ -0,0 +1,12 @@
+package com.upc.gessi.qrapids.app.domain.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StrategicIndicatorsTest {
+
+    @Test
+    public void getStrategicIndicatorsEvaluation() {
+    }
+}
