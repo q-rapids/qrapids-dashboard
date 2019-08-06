6d43ab237a7c6d065ebf4025741d2fcb62170c29
Aleix Balletbó <aleixballetbo@gmail.com>
2019-08-06 21:55:38 +0200

    Route Repository - Current status tests

---
+++ src/test/java/com/upc/gessi/qrapids/app/domain/repositories/Route/RouteRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.upc.gessi.qrapids.app.domain.repositories.Route;
+
+import com.upc.gessi.qrapids.app.domain.models.Route;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ComponentScan("com.upc.gessi.qrapids.app.database.repositories")
+@DataJpaTest
+public class RouteRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private RouteRepository routeRepository;
+
+    @Test
+    public void findByName() {
+        // Given
+        String route1Name = "REST : Strategic Indicators - Current Evaluation";
+        String route1Path = "/CurrentEvaluation";
+        Route route1 = new Route(route1Name, route1Path);
+        entityManager.persist(route1);
+
+        String route2Name = "REST : Strategic Indicators - Historical Data";
+        String route2Path = "/HistoricalData";
+        Route route2 = new Route(route2Name, route2Path);
+        entityManager.persistAndFlush(route2);
+
+        // When
+        Route routeFound = routeRepository.findByName(route1Name);
+
+        // Then
+        assertEquals(route1, routeFound);
+    }
+}
