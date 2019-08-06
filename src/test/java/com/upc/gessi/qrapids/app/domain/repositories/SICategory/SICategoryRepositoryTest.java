8b402576d6ab345e763cde15503742c61b02c8e4
Aleix Balletbó <aleixballetbo@gmail.com>
2019-08-06 22:19:07 +0200

    SICategory Repository - Current status tests

---
+++ src/test/java/com/upc/gessi/qrapids/app/domain/repositories/SICategory/SICategoryRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.upc.gessi.qrapids.app.domain.repositories.SICategory;
+
+import com.upc.gessi.qrapids.app.domain.models.SICategory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ComponentScan("com.upc.gessi.qrapids.app.database.repositories")
+@DataJpaTest
+public class SICategoryRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private SICategoryRepository siCategoryRepository;
+
+    @Test
+    public void findByName() {
+        // Given
+        String strategicIndicatorGoodCategoryName = "Good";
+        String strategicIndicatorGoodCategoryColor = "#00ff00";
+        SICategory siGoodCategory = new SICategory(strategicIndicatorGoodCategoryName, strategicIndicatorGoodCategoryColor);
+        entityManager.persist(siGoodCategory);
+
+        String strategicIndicatorNeutralCategoryName = "Neutral";
+        String strategicIndicatorNeutralCategoryColor = "#ff8000";
+        SICategory siNeutralCategory = new SICategory(strategicIndicatorNeutralCategoryName, strategicIndicatorNeutralCategoryColor);
+        entityManager.persist(siNeutralCategory);
+
+        String strategicIndicatorBadCategoryName = "Bad";
+        String strategicIndicatorBadCategoryColor = "#ff0000";
+        SICategory siBadCategory = new SICategory(strategicIndicatorBadCategoryName, strategicIndicatorBadCategoryColor);
+        entityManager.persistAndFlush(siBadCategory);
+
+        // When
+        SICategory siCategoryFound = siCategoryRepository.findByName(strategicIndicatorGoodCategoryName);
+
+        // Then
+        assertEquals(siGoodCategory, siCategoryFound);
+    }
+}
