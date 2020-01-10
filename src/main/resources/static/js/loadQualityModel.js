7cef12696e7039a6e8f18a3a95f712ce7d343f49
AlexV <v.alexandra.v@gmail.com>
2020-01-10 13:02:22 +0100

    Solve issue #116

--- src/main/resources/static/js/loadQualityModel.js
+++ src/main/resources/static/js/loadQualityModel.js
@@ -38,6 +38,7 @@ function buildTree(strategicIndicators) {
                 qmnodes.set(factor.id, node);
             if (!qmedges.has(factor.id+"-"+strategicIndicator.id))
                 qmedges.set(factor.id+"-"+strategicIndicator.id, createEdge(factor, strategicIndicator));
+            var metricsWeights = sumMetricsWeights(factor.metrics);
 
             for (var k = 0; k < factor.metrics.length; k++) {
                 var metric = factor.metrics[k];
@@ -45,17 +46,30 @@ function buildTree(strategicIndicators) {
                 if (!qmnodes.has(metric.id))
                     qmnodes.set(metric.id, node);
                 if (!qmedges.has(metric.id+"-"+factor.id))
-                    qmedges.set(metric.id+"-"+factor.id, createEdge(metric, factor));
+                    qmedges.set(metric.id+"-"+factor.id, createEdge(metric, factor, metricsWeights));
             }
         }
     }
     displayData(Array.from(qmnodes.values()), Array.from(qmedges.values()));
 }
 
+function sumMetricsWeights(elements){
+    var totalWeight = 0;
+    for (var i = 0; i < elements.length; i++){
+        totalWeight += parseFloat(elements[i].weight);
+    }
+    return totalWeight;
+}
+
 function createNode (element, color, colorBorder) {
     var value;
     if (element.valueDescription) value = element.valueDescription;
-    else value = element.value;
+    else {
+        if (element.weight && element.weight != 0) {
+            var w = parseFloat(element.weight);
+            value = parseFloat(element.value)/w;
+        } else value = element.value;
+    }
     return {
         data: {
             id: element.id,
@@ -66,10 +80,12 @@ function createNode (element, color, colorBorder) {
     }
 }
 
-function createEdge (source, target) {
+function createEdge (source, target, aux) { // aux = sum metrics weights
     var weight = source.weight;
-    if (weight == 0) weight = null;
-    else weight = parseFloat(weight).toFixed(0);
+    if (aux) {
+        weight = ((parseFloat(weight)/aux) * 100).toFixed(0) + "%"; // weight percentage
+    }
+    else weight = null; // it's indifferent which weight has factor, we won't show it
     return {
         data: {
             source: source.id,
